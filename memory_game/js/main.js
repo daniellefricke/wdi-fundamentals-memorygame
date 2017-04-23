d53d03f06478fb8b7f91375204db2ca0a8e1faee
Danielle Fricke <daniellefricke@Danielles-MacBook-Pro.local>
2017-04-23 15:11:09 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,8 +1,34 @@
 
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "king";
-var cardFour = "queen";
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = ["cardOne" && "cardTwo"];
+var checkForMatch = function () {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		console.log("You found a match!");
+	} else {
+		console.log("Sorry, try again.");
+	}
+};
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+var flipCard = function (cardId) {
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1] || cardsInPlay.length === 2 && cardsInPlay[2] === cardsInPlay[3]){
+	alert("You found a match!");
+} else {
+	alert("Sorry, try again");
+}
+};
+
+
+flipCard(0)
+flipCard(2)
+checkForMatch()
+
+
+
+
+
+
+
+
+
