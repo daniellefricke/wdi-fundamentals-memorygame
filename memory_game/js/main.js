7f7c0637e9d21cb80bd9aecb0a255e176646e563
Danielle Fricke <daniellefricke@Danielles-MacBook-Pro.local>
2017-04-27 12:57:29 -0400

    creted game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -24,6 +24,7 @@ var cards = [
 
 var cardsInPlay = ["cardOne" && "cardTwo"];
 var checkForMatch = function () {
+	this.setAttribute('src'(cards[cardId].cardImage));
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 		console.log("You found a match!");
 	} else {
@@ -31,16 +32,29 @@ var checkForMatch = function () {
 	}
 };
 
-var flipCard = function (cardId) {
+var flipCard = function () {
+	this.getAttribute('data-id', 'cardID');
 	console.log("User flipped " + cards[cardId].rank);
 	cardsInPlay.push(cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
 };
 
-flipCard(0);
-flipCard(2);
-checkForMatch()
+var createBoard = function () {
+	for (var i = 0; i < cards.length; i++){
+
+	var cardElement = document.createElement('img');
+	cardElement.setAttribute('src', 'images/back.png');
+	cardElement.setAttribute('data-id', i);
+	cardElement.addEventListener("click", 'flipCard'); {
+
+	};
+
+	cardElement.appendChild('game-board');
+	}
+};
+
+createBoard();
 
 
 
